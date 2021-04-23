0e797f33c18b2a4d3bd6862e8cc1bcc8e1360c9e
David Lima <antdavidlima@gmail.com>
2021-04-22 23:57:10 -0300

    Insertion sort algorithm

---
+++ insertion_sort.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+// A - Array to be sorted
+// n - Length of A
+void insertionSort(int [], int);
+
+
+int main() {
+  int size;
+  printf("Array size: ");
+  scanf("%i", &size);
+
+  int array[size];
+
+  printf("Unsorted array:\n");
+  for (int i = 0; i < size; i++) {
+    array[i] = rand() % 100;
+    printf("%i ", array[i]);
+  }
+
+  printf("\n");
+
+  printf("Sorted array:\n");
+  insertionSort(array, size);
+
+  for (int i = 0; i < size; i++) {
+    printf("%i ", array[i]);
+  }
+
+  printf("\n");
+
+  return 0;
+}
+
+void insertionSort(int A[], int n) {
+  for (int i = 0; i < n; i++) {
+    int current = A[i];
+    int j = i - 1;
+    while (j >= 0 && A[j] > current) {
+      A[j + 1] = A[j];
+      j--;
+    }
+    A[j + 1] = current;
+  }
+}
