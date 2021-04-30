d4f3cff0b7e47452521d162761e4f3929d020b2c
David Lima <antdavidlima@gmail.com>
2021-04-30 15:54:36 -0300

    Array linear search

---
+++ arrays.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void linearSearch(int [], int, int);
+
+int main(int argc, char const *argv[]) {
+  int size;
+  printf("Array size: ");
+  scanf("%i", &size);
+
+  int array[size];
+
+  printf("Array:\n");
+  for (int i = 0; i < size; i++) {
+    array[i] = rand() % 100;
+    printf("%i ", array[i]);
+  }
+  printf("\n");
+
+  int toFind;
+  printf("Value to be searched:\n");
+  scanf("%i", &toFind);
+
+  linearSearch(array, size, toFind);
+  
+  return 0;
+}
+
+void linearSearch(int A[], int n, int x)
+{
+  int i = 0;
+  while (i < n && A[i] != x)
+  {
+    i++;
+  }
+  if (i == n)
+  {
+    printf("value not found\n");
+  }
+  else
+  {
+    printf("value found in position: %i\n", i);
+  }
+  
+}
